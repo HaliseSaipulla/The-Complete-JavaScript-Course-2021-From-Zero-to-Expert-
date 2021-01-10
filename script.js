2101f55dde3c9a6a784cf064d8c3c89ffdca8f04
lisa0015 <lisasa0215@gmail.com>
2021-01-09 16:49:08 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// let  js =('amazing')
+// if (js==='amazing') alert('JavaScript is fun!');
+// console.log(50+59+4*6);
+
+// var age=32;
+// console.log(age)
+// age =33;
+// console.log(age);
+
+
+/*const massMark = 95;
+const heighMark = 1.88;
+const massJohn = 85;
+const heighJohn = 1.76;
+const BMIMark = massMark/heighMark **2;
+const BMIJohn = massJohn/heighJohn **2;
+const markHigherBMI = BMIJohn<BMIMark;
+if(BMIMark>BMIJohn){
+      console.log("Mark's BMI is higher than John's");
+}else{
+      console.log("John's BMI is higher than Mark's");
+}*/
+prompt("What is your fav num?")
