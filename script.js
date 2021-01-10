78ca1a9965cfa630914c2c43c73984af37370cc7
lisa0015 <lisasa0215@gmail.com>
2021-01-10 12:08:22 -0800

    fifth

--- script.js
+++ script.js
@@ -22,29 +22,31 @@ if(BMIMark>BMIJohn){
 }*/
 // prompt("What is your fav num?")
 // switch
-const day='monday';
-switch(day){
-      case 'monday':
-            console.log('Plan course structure');
-            console.log('Go to coding meeting');
-            break;
-      case 'tuesday':
-            console.log('Prepare theory videos');
-            break;
-      case 'wednesday':
-      case 'thursday':
-            console.log('write code examples');
-            break;
-      case 'friday':
-            console.log('Record videos');
-            break;
-      case 'saturday':
-      case 'sunday':
-            console.log('Enjoy the weekend :D');
-            break;
-      default:
-            console.log('Not a valid day!');
-}
+// const day='monday';
+// switch(day){
+//       case 'monday':
+//             console.log('Plan course structure');
+//             console.log('Go to coding meeting');
+//             break;
+//       case 'tuesday':
+//             console.log('Prepare theory videos');
+//             break;
+//       case 'wednesday':
+//       case 'thursday':
+//             console.log('write code examples');
+//             break;
+//       case 'friday':
+//             console.log('Record videos');
+//             break;
+//       case 'saturday':
+//       case 'sunday':
+//             console.log('Enjoy the weekend :D');
+//             break;
+//       default:
+//             console.log('Not a valid day!');
+// }
 
-        
-
+const age = 23;
+// age>=18 ?  console.log('I like to drink wine'):console.log('I like to drink water');     
+const drink = age>=18?'wine':'water';
+console.log(drink);
