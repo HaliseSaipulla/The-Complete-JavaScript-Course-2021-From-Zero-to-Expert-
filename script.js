93c003d11bf581257a00212bf9c317d2bbc90753
lisa0015 <lisasa0215@gmail.com>
2021-01-10 11:50:05 -0800

    forth

--- script.js
+++ script.js
@@ -20,5 +20,31 @@ if(BMIMark>BMIJohn){
 }else{
       console.log("John's BMI is higher than Mark's");
 }*/
-prompt("What is your fav num?")
-gfjgkajak
+// prompt("What is your fav num?")
+// switch
+const day='monday';
+switch(day){
+      case 'monday':
+            console.log('Plan course structure');
+            console.log('Go to coding meeting');
+            break;
+      case 'tuesday':
+            console.log('Prepare theory videos');
+            break;
+      case 'wednesday':
+      case 'thursday':
+            console.log('write code examples');
+            break;
+      case 'friday':
+            console.log('Record videos');
+            break;
+      case 'saturday':
+      case 'sunday':
+            console.log('Enjoy the weekend :D');
+            break;
+      default:
+            console.log('Not a valid day!');
+}
+
+        
+
